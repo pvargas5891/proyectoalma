af823980e2f4eaa62f5a1eaf58cb953579a7c415
Pablo Vargas Figueroa <pvargas@sentra.cl>
2017-08-04 01:32:52 -0400

    pantalla 1 ok

--- scripts/controller/homeCtrl.js
+++ scripts/controller/homeCtrl.js
@@ -25,4 +25,26 @@ function homeCtrl($scope,factoryTest,LayouHomeService2,Session){
     $scope.micuenta = layout.texto9;
     $scope.cerrarSesion = layout.texto10;
     $scope.notification = layout.texto11;
+    $scope.centrodecostos = layout.texto12;
+    $scope.resumenMisDatos = layout.texto13;
+    $scope.resumenFacturacion = layout.texto14;
+    $scope.resumenTrafico = layout.texto15;
+    $scope.resumenMisPagos = layout.texto16;
+    $scope.celular= layout.texto17;
+    $scope.prip1= layout.texto18;
+    $scope.miplan= layout.texto19;
+    $scope.ciclo= layout.texto20;
+    $scope.estado= layout.texto21;
+    $scope.vencimiento= layout.texto22;
+    $scope.totalapagar= layout.texto23;
+    $scope.periodo1= layout.texto24;
+    $scope.prip2= layout.texto25;
+    $scope.voz= layout.texto26;
+    $scope.sms= layout.texto27;
+    $scope.internet= layout.texto28;
+    $scope.periodo2= layout.texto29;
+    $scope.saldo= layout.texto30;
+    $scope.saldoanterior= layout.texto31;
+    $scope.total= layout.texto32;
+
 }
